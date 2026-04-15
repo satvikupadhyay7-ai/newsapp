2a9a0a2679c622e335060d0e0fc292fa7fbb205c
satvikupadhyay7-ai <satvikupadhyay7@gmail.com>
2026-04-15 21:42:29 +0530

    Update NewsItem.jsx

--- NewsItem.jsx
+++ NewsItem.jsx
@@ -1,13 +1,26 @@
-const NewsItem=()=>{
-    return(<div>
-<div className="card" style={{maxwidth:"345px"}}>
-  <img src={src} className="card-img-top" alt="...">
-  <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{desciption}</p>
-    <a href={url} className="btn btn-primary">Read More</a>
-  </div>
-</div>
-    </div>)
+import image from '../assets/dolla.jpeg'
+const NewsItem = ({ title, description, src, url }) => {
+  return (
+    <div>
+      <div className="card bg-dark text-lieght mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{ maxWidth: "345px" }}>
+        
+        <img src={src?src:image} style={{height:20px width :30px}} className="card-img-top" alt="news" />
+
+        <div className="card-body">
+          <h5 className="card-title">{title.slice(0,50)}</h5>
+
+          <p className="card-text">
+            {description ? description.slice(0,90):"hi"}
+          </p>
+
+          <a href={url} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
+            Read More
+          </a>
+        </div>
+
+      </div>
+    </div>
+  )
 }
+
 export default NewsItem
